8a66c5d0f67a2a12846e75d0d2d5c2b9ece92349
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-27 14:57:29 -0400

    modify so that command is hashed with ip

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -3,34 +3,34 @@ import { commandHandler } from "../ai/command";
 import { bestModel } from "../ai/model";
 
 export const parseCommand = async (
-  command: string,
-  ip: string,
-  userId: string
+    command: string,
+    ip: string,
+    userId: string
 ) => {
-  const hash = await Bun.hash(command);
-  // if this hash doesn't exist
-  // create it
-  // run the command associated with the hash
+    const hash = await Bun.hash(command + ip).toString();
+    // if this hash doesn't exist
+    // create it
+    // run the command associated with the hash
 
-  if (!(await redis.exists(hash.toString()))) {
-    const commandOutput = await runCommand(command);
-    redis.set(hash.toString(), commandOutput);
-    return commandOutput;
-  } else {
-    const commandOutput = await commandHandler({
-      command,
-      machineId: ip,
-      userId,
-      model: bestModel,
-    });
-    return commandOutput;
-  }
+    if (!(await redis.exists(hash))) {
+        const commandOutput = await runCommand(command);
+        redis.set(hash.toString(), commandOutput);
+        return commandOutput;
+    } else {
+        const commandOutput = await commandHandler({
+            command,
+            machineId: ip,
+            userId,
+            model: bestModel,
+        });
+        return commandOutput;
+    }
 };
 
 const runCommand = async (command: string) => {
-  return "dummy command data for command: " + command;
+    return "dummy command data for command: " + command;
 };
 
 const getData = async (hash: string) => {
-  return await redis.get(hash);
+    return await redis.get(hash);
 };
