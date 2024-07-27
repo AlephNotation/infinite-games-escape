41f52ee0ba7ae8e6626edcdd58067ed6cb205bd1
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-27 16:14:43 -0400

    fetch request works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { cors } from 'hono/cors'
+
 import { commandRouter } from "./routes/command.route";
 import { chatMessagesRouter } from "./routes/chatMessages.route";
 
@@ -7,6 +9,11 @@ const app = new Hono();
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+
+// Apply CORS middleware to all routes
+app.use('*', cors())
+
 
 app.route("", commandRouter);
 app.route("", chatMessagesRouter);
