06bb9ee76c87a8aed7206bd142c99410608fdf87
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-29 16:45:50 -0400

    cleanup old build shit

--- backend/src/controllers/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var redisClient_1 = require("../../utils/redisClient");
-console.log("hello bro");
-redisClient_1.default.get("2164169596638794122n");
--- backend/utils/redisClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ioredis_1 = require("ioredis");
-var redis = new ioredis_1.default(process.env.REDIS_URL || 'redis://localhost:6379');
-exports.default = redis;
