36b790151f6e7b87e1b642a06e18a0ceabab23b5
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-29 16:46:47 -0400

    actually cleanup this time

--- backend/src/ai/model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bestModel = exports.fastModel = void 0;
-var anthropic_1 = require("@ai-sdk/anthropic");
-exports.fastModel = (0, anthropic_1.anthropic)("claude-3-haiku-20240307");
-exports.bestModel = (0, anthropic_1.anthropic)("claude-3-opus-20240229");
